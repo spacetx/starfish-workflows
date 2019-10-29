0787ac6b25782021cba435275e90d61eb24321e9
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2019-10-29 11:27:10 -0700

    skeleton code for potential new batch job

--- aws-batch-job/recipe.py
+++ aws-batch-job/recipe.py
@@ -1,13 +1,8 @@
 import starfish
-from starfish import FieldOfView
-from starfish.image import Filter
-from starfish.image import ApplyTransform, LearnTransform
-from starfish.spots import DecodeSpots, FindSpots
-from starfish.types import Axes, FunctionSource
 
 
 def process_fov(field_num: int, experiment_str: str):
-    """Process a single field of view of ISS data
+    """Process a single field of view of data
     Parameters
     ----------
     field_num : int
@@ -21,48 +16,7 @@ def process_fov(field_num: int, experiment_str: str):
         tabular object containing the locations of detected spots.
     """
 
-    fov_str: str = f"fov_{int(field_num):03d}"
-
-    # load experiment
-    experiment = starfish.Experiment.from_json(experiment_str)
-
-    print(f"loading fov: {fov_str}")
-    fov = experiment[fov_str]
-
-    # note the structure of the 5D tensor containing the raw imaging data
-    imgs = fov.get_image(FieldOfView.PRIMARY_IMAGES)
-    dots = fov.get_image("dots")
-    nuclei = fov.get_image("nuclei")
-
-    print("Learning Transform")
-    learn_translation = LearnTransform.Translation(reference_stack=dots, axes=Axes.ROUND, upsampling=1000)
-    transforms_list = learn_translation.run(imgs.reduce({Axes.CH, Axes.ZPLANE}, func="max"))
-
-    print("Applying transform")
-    warp = ApplyTransform.Warp()
-    registered_imgs = warp.run(imgs, transforms_list=transforms_list, verbose=True)
-
-    print("Filter WhiteTophat")
-    filt = Filter.WhiteTophat(masking_radius=15, is_volume=False)
-
-    filtered_imgs = filt.run(registered_imgs, verbose=True)
-    filt.run(dots, verbose=True, in_place=True)
-    filt.run(nuclei, verbose=True, in_place=True)
-
-    print("Detecting")
-    detector = FindSpots.BlobDetector(
-        min_sigma=1,
-        max_sigma=10,
-        num_sigma=30,
-        threshold=0.01,
-        measurement_type='mean',
-    )
-    dots_max = dots.reduce((Axes.ROUND, Axes.ZPLANE), func="max", module=FunctionSource.np)
-    spots = detector.run(image_stack=filtered_imgs, reference_image=dots_max)
-
-    print("Decoding")
-    decoder = DecodeSpots.PerRoundMaxChannel(codebook=experiment.codebook)
-    decoded = decoder.run(spots=spots)
+    # TODO ADD RECIPE STEPS HERE, END WITH DECODED INTENSITY TABLE NAMES DECODED
 
     df = decoded.to_decoded_dataframe()
     return df
--- aws-batch-job/runner.sh
+++ aws-batch-job/runner.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 field_of_view=$AWS_BATCH_JOB_ARRAY_INDEX
-experiment=https://s3.amazonaws.com/spacetx.starfish.data.public/browse/formatted/iss/20190506/experiment.json
+experiment=REPLACE_WITH_URL_TO_EXPERIEMENT
 
 python - << EOF
 import starfish
 recipe = __import__('recipe')
-
 decoded_spots = recipe.process_fov(${field_of_view}, "${experiment}")
 filename = f"fov_{int(${field_of_view}):03d}"+"_decoded.csv"
 print(filename)
 decoded_spots.save_csv(filename)
 EOF
 
-aws s3 cp .  s3://starfish.data.output-warehouse/batch-job-outputs/iss-published/ --recursive  --exclude "*" --include "*decoded.csv"
+aws s3 cp .  REPLACE_WITH_PATH_TO_S3_BUCKET_FOR_RESULTS --recursive  --exclude "*" --include "*decoded.csv"
