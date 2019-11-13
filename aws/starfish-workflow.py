88ea353f1197b80f5c313fd2884b9f52ee840d26
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2019-11-13 14:56:48 -0800

    updates to aws workflow

--- aws/starfish-workflow.py
+++ aws/starfish-workflow.py
@@ -1,32 +1,76 @@
-import subprocess
-import json
+import argparse
+import boto3
+
+batch = boto3.client('batch')
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--experiment-url", help="The url to your experiment.json file", type=str)
+parser.add_argument("--num-fovs", help="The number of fields of view in the experiment", type=int)
+parser.add_argument("--recipe-location", help="The location of the recipe file to process "
+                                              "the experiment with", type=str)
+parser.add_argument("--results-bucket", help="The s3 bucket to copy the results to", type=str)
+args = parser.parse_args()
 
 
-# Submit array job
-submitCommand = "aws --profile spacetx-admin batch submit-job --cli-input-json file://submit-array-job.json"
-process = subprocess.Popen(submitCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-submit_info = json.loads(output)
-
-# get jobID for merge job
-job_id = submit_info["jobId"]
-
-
-# Add Job ID to merge-results params json
-with open('aws-merge-results-job/submit-merge-job.json', "r+") as json_file:
-    merge_job_params = json.load(json_file)
-    merge_job_params["dependsOn"] = [{"jobId": job_id}]
-    json_file.seek(0)  # <--- should reset file position to the beginning.
-    json.dump(merge_job_params, json_file, indent=4)
-    json_file.truncate()
+def main():
+    experiment_location = args.experiment_url
+    num_fovs = args.num_fovs
+    recipe_location = args.recipe_location
+    results_bucket = args.results_bucket
+    process_job_id = submit_array_job(experiment_location=experiment_location,
+                                      num_fovs=num_fovs,
+                                      recipe_location=recipe_location,
+                                      results_bucket=results_bucket)
+    print(f"Process fovs array job {process_job_id} successfully submitted.")
+    merge_job_id = submit_merge_job(job_id=process_job_id, results_bucket=results_bucket)
+    print(f"Merge results job {merge_job_id} successfully submitted.")
 
 
-# Submit merge job
-submitCommand = "aws --profile spacetx-admin batch submit-job --cli-input-json file://submit-merge-job.json"
-process = subprocess.Popen(submitCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-submit_info = json.loads(output)
+def submit_array_job(experiment_location, num_fovs, recipe_location, results_bucket):
+    submitJobResponse = batch.submit_job(
+        jobName="process-fov-batch-job",
+        jobQueue="first-run-job-queue",
+        jobDefinition="process-fov",
+        arrayProperties={"size": num_fovs},
+        containerOverrides={
+            "environment": [
+                {
+                    "name": "RECIPE_LOCATION",
+                    "value": recipe_location
+                },
+                {
+                    "name": "EXPERIMENT_URL",
+                    "value": experiment_location
+                },
+                {
+                    "name": "RESULTS_LOCATION",
+                    "value": results_bucket
+                  }
+                ]
+              }
+            )
 
-print(submit_info)
+    return submitJobResponse['jobId']
 
 
+def submit_merge_job(job_id, results_bucket):
+    submitJobResponse = batch.submit_job(
+        jobName="merge-results-job",
+        jobQueue="first-run-job-queue",
+        jobDefinition="merge-job",
+        dependsOn=[{"jobId": job_id}],
+        containerOverrides={
+            "environment": [
+                {
+                    "name": "RESULTS_LOCATION",
+                    "value": results_bucket
+                }
+            ]
+        }
+    )
+
+    return submitJobResponse['jobId']
+
+
+if __name__ == '__main__':
+    main()
